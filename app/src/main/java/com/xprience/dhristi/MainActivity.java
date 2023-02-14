99a8dc92acd78763d68b43c84e87e3d9e9ea7317
siddheswarojha <siddheswarojha23@gmail.com>
2023-02-14 07:31:56 +0530

    basic implementation of app prototype todo api integration

--- app/src/main/java/com/xprience/dhristi/MainActivity.java
+++ app/src/main/java/com/xprience/dhristi/MainActivity.java
@@ -6,20 +6,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView iv_mic;
-    private TextView tv_Speech_to_text;
+    private ImageView iv_mic,iv_mic1;
+    private TextView tv_Speech_to_text,tv_Speech_to_text1;
     private static final int REQUEST_CODE_SPEECH_INPUT = 1;
+    private static final int REQUEST_CODE_SPEECH_INPUT1 = 1;
+    private Button btnSendMail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +42,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         iv_mic = findViewById(R.id.iv_mic);
         tv_Speech_to_text = findViewById(R.id.tv_speech_to_text);
+
+        iv_mic1 = findViewById(R.id.iv_mic1);
+        tv_Speech_to_text1 = findViewById(R.id.tv_speech_to_text1);
+        btnSendMail = findViewById(R.id.btnSend);
 
         iv_mic.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,7 +67,74 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        iv_mic1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent
+                        = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
+                        Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to text");
+
+                try {
+                    startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT1);
+                }
+                catch (Exception e) {
+                    Toast.makeText(MainActivity.this, " " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        btnSendMail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String receiver = tv_Speech_to_text.getText().toString().trim();
+                String mailBody = tv_Speech_to_text1.getText().toString();
+                sendMail(receiver,mailBody);
+
+            }
+        });
     }
+
+    private void sendMail(String receiver, String mailBody) {
+
+        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+        //todo api integration
+        String url = "";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                try {
+                    Log.d("message","mailSent");
+
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("API_LOG",error.toString());
+            }
+        }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("recipient",receiver);
+                params.put("body",mailBody);
+                return params;
+            }
+        };
+        requestQueue.add(stringRequest);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode,
                                     @Nullable Intent data)
@@ -60,8 +146,24 @@ public class MainActivity extends AppCompatActivity {
                         RecognizerIntent.EXTRA_RESULTS);
                 tv_Speech_to_text.setText(
                         Objects.requireNonNull(result).get(0));
+
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_SPEECH_INPUT1) {
+            if (resultCode == RESULT_OK && data != null) {
+                ArrayList<String> result = data.getStringArrayListExtra(
+                        RecognizerIntent.EXTRA_RESULTS);
+                tv_Speech_to_text1.setText(
+                        Objects.requireNonNull(result).get(0));
+
             }
         }
     }
+
+
 }
+
+
 
